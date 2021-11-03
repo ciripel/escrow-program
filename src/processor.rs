ab0487de40318c6473fb7e6cf90d194d2a9e039f
ciripel <nedelcu.alexandru@yahoo.com>
2021-11-03 23:18:25 +0200

    at process_init_escrow

--- src/processor.rs
+++ src/processor.rs
@@ -1,8 +1,58 @@
 //! Program state processor
 
-use crate::error::EscrowError;
+use crate::{error::EscrowError, instruction::EscrowInstruction};
 use num_traits::FromPrimitive;
-use solana_program::{decode_error::DecodeError, msg, program_error::PrintProgramError};
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    decode_error::DecodeError,
+    entrypoint::ProgramResult,
+    msg,
+    program_error::{PrintProgramError, ProgramError},
+    pubkey::Pubkey,
+};
+
+pub struct Processor;
+impl Processor {
+    pub fn process(
+        program_id: &Pubkey,
+        accounts: &[AccountInfo],
+        instruction_data: &[u8],
+    ) -> ProgramResult {
+        let instruction = EscrowInstruction::unpack(instruction_data)?;
+
+        match instruction {
+            EscrowInstruction::InitEscrow { amount } => {
+                msg!("Instruction: InitEscrow");
+                Self::process_init_escrow(accounts, amount, program_id)
+            }
+        }
+    }
+
+    fn process_init_escrow(
+        accounts: &[AccountInfo],
+        amount: u64,
+        program_id: &Pubkey,
+    ) -> ProgramResult {
+        let account_info_iter = &mut accounts.iter();
+        let initializer = next_account_info(account_info_iter)?;
+
+        if !initializer.is_signer {
+            return Err(ProgramError::MissingRequiredSignature);
+        }
+
+        let temp_token_account = next_account_info(account_info_iter)?;
+
+        let token_to_receive_account = next_account_info(account_info_iter)?;
+
+        if *token_to_receive_account.owner != spl_token::id() {
+            return Err(ProgramError::IncorrectProgramId);
+        }
+
+        let escrow_account = next_account_info(account_info_iter)?;
+
+        Ok(())
+    }
+}
 
 impl PrintProgramError for EscrowError {
     fn print<E>(&self)

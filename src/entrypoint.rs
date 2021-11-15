86b11297224959a2828c2a5450ab64ff5a613121
ciripel <nedelcu.alexandru@yahoo.com>
2021-11-15 21:32:20 +0200

    fixed error printing and removed testing

--- src/entrypoint.rs
+++ src/entrypoint.rs
@@ -1,10 +1,11 @@
 //! Program entrypoint
 
 use solana_program::{
-    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, msg, pubkey::Pubkey,
+    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult,
+    program_error::PrintProgramError, pubkey::Pubkey,
 };
 
-use crate::processor::Processor;
+use crate::{error::EscrowError, processor::Processor};
 
 entrypoint!(process_instruction);
 fn process_instruction(
@@ -12,38 +13,10 @@ fn process_instruction(
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
-    Processor::process(program_id, accounts, instruction_data)
-}
-
-#[cfg(test)]
-mod test {
-    use {
-        super::*,
-        assert_matches::*,
-        solana_program::instruction::{AccountMeta, Instruction},
-        solana_program_test::*,
-        solana_sdk::{signature::Signer, transaction::Transaction},
-    };
-
-    #[tokio::test]
-    async fn test_transaction() {
-        let program_id = Pubkey::new_unique();
-
-        let (mut banks_client, payer, recent_blockhash) =
-            ProgramTest::new("solana_escrow", program_id, processor!(process_instruction))
-                .start()
-                .await;
-
-        let mut transaction = Transaction::new_with_payer(
-            &[Instruction {
-                program_id,
-                accounts: vec![AccountMeta::new(payer.pubkey(), false)],
-                data: vec![1, 2, 3],
-            }],
-            Some(&payer.pubkey()),
-        );
-        transaction.sign(&[&payer], recent_blockhash);
-
-        assert_matches!(banks_client.process_transaction(transaction).await, Ok(()));
+    if let Err(error) = Processor::process(program_id, accounts, instruction_data) {
+        // catch the error so we can print it
+        error.print::<EscrowError>();
+        return Err(error);
     }
+    Ok(())
 }
